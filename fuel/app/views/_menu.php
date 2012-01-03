8158b4047594680bf2493e0564ab03fcce467c93
Sébastien Drouyer <sdrdis@gmail.com>
2012-01-03 12:03:46 +0100

    petite correction...

--- fuel/app/views/_menu.php
+++ fuel/app/views/_menu.php
@@ -1,4 +1,4 @@
 <div id="menu">
-    <?= Html::anchor('notes/index', 'Notes', array('class' => 'item '.(\Request::active()->controller == '\Controller_Notes' ? 'active' : ''))) ?>
-    <?= Html::anchor('person/index', 'Adresses et numéros', array('class' => 'item '.(\Request::active()->controller == '\Controller_Person' ? 'active' : ''))) ?>
+    <?= Html::anchor('notes/index', 'Notes', array('class' => 'item '.(Request::active()->controller == '\Controller_Notes' ? 'active' : ''))) ?>
+    <?= Html::anchor('person/index', 'Adresses et numéros', array('class' => 'item '.(Request::active()->controller == '\Controller_Person' ? 'active' : ''))) ?>
 </div>
