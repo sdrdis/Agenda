8f4e734c29901c543ec93d3b8cbabed4c3254bc3
Sébastien Drouyer <sdrdis@gmail.com>
2012-01-17 11:45:39 +0100

    Modification pour que l'application ne dépende pas de l'option short_open_tag pour PHP < 5.4.

--- fuel/app/views/_menu.php
+++ fuel/app/views/_menu.php
@@ -1,4 +1,4 @@
 <div id="menu">
-    <?= Html::anchor('notes/index', 'Notes', array('class' => 'item '.(\Request::active()->controller == 'Controller_Notes' ? 'active' : ''))) ?>
-    <?= Html::anchor('person/index', 'Adresses et numéros', array('class' => 'item '.(\Request::active()->controller == 'Controller_Person' ? 'active' : ''))) ?>
+    <?php echo Html::anchor('notes/index', 'Notes', array('class' => 'item '.(\Request::active()->controller == 'Controller_Notes' ? 'active' : ''))) ?>
+    <?php echo Html::anchor('person/index', 'Adresses et numéros', array('class' => 'item '.(\Request::active()->controller == 'Controller_Person' ? 'active' : ''))) ?>
 </div>
